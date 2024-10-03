abb9e64ff8f51ab5d7a46d39dc2a37d3af8811f0
Pradumn Mandal <97424547+pradumn1999@users.noreply.github.com>
2024-10-03 18:46:28 +0530

    cc

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -26,7 +26,7 @@ const Signup = () => {
       setLogin(true); // Switch to login view after successful signup
     } catch (error) {
       toast.error('Error during form submission');
-      console.error('Error during form submission:', error);
+      console.error('Error during form submission:',error);
     }
   };
 
